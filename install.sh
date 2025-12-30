25e3a4fc25c8fdcb0654fb181715e45c87e14b58
Kyan Jeuring <kyanjeuring@gmail.com>
2025-12-30 11:53:26 +0100

    Updated color in install.sh

--- install.sh
+++ install.sh
@@ -4,20 +4,41 @@ set -euo pipefail
 # ==================================================
 # Colors / output helpers
 # ==================================================
+
 if [[ -t 1 ]]; then
+  BLACK='\033[0;30m'
   RED='\033[0;31m'
   GREEN='\033[0;32m'
   YELLOW='\033[0;33m'
   BLUE='\033[0;34m'
+  MAGENTA='\033[0;35m'
+  CYAN='\033[0;36m'
+  WHITE='\033[0;37m'
+
+  LIGHT='\033[1m'
+  UNDERLINE='\033[4m'
+  REVERSE='\033[7m'
   NC='\033[0m'
 else
-  RED='' GREEN='' YELLOW='' BLUE='' NC=''
+  BLACK=''
+  RED=''
+  GREEN=''
+  YELLOW=''
+  BLUE=''
+  MAGENTA=''
+  CYAN=''
+  WHITE=''
+
+  LIGHT=''
+  UNDERLINE=''
+  REVERSE=''
+  NC=''
 fi
 
-OK="${GREEN}[OK]${NC}"
-INFO="${BLUE}[INFO]${NC}"
-WARN="${YELLOW}[WARN]${NC}"
+OK="${GREEN}${LIGHT}[OK]${NC}"
 ERR="${RED}[ERROR]${NC}"
+INFO="${BLUE}${LIGHT}[INFO]${NC}"
+WARN="${YELLOW}[WARN]${NC}"
 
 # ==================================================
 # Paths
