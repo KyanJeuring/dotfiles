f1ec9399e93c9682728a38e6ad22cace5ae511bd
Kyan Jeuring <kyanjeuring@gmail.com>
2026-01-09 09:37:32 +0100

    Updated file tree neovim logic

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,27 +22,47 @@ vim.opt.rtp:prepend(lazypath)
 -- ==================================================
 
 require("lazy").setup({
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = false,
+                },
+            })
 
-  -- File tree
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = false,
-        },
-      })
+            -- Keybind: <leader>e to toggle tree
+            vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
 
-      -- Keybind: <leader>e to toggle tree
-      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-    end,
-  },
+            -- ==================================================
+            -- Auto-open tree on startup
+            -- ==================================================
+
+            vim.api.nvim_create_autocmd("VimEnter", {
+                callback = function(data)
+                    -- Buffer is a file
+                    local is_file = vim.fn.filereadable(data.file) == 1
+                    -- Buffer is a directory
+                    local is_dir = vim.fn.isdirectory(data.file) == 1
+
+                    if is_dir then
+                    -- Change to the directory and open tree
+                    vim.cmd.cd(data.file)
+                    require("nvim-tree.api").tree.open()
+                    elseif is_file then
+                    -- Open tree but keep focus on file
+                    require("nvim-tree.api").tree.open({ focus = false })
+                    end
+                end,
+            })
+        end,
+    },
 
 })
