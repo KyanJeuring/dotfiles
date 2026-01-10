28f0fbe328eac709c33aa0ca1bf2279a92da9429
Kyan Jeuring <kyanjeuring@gmail.com>
2026-01-10 16:53:27 +0100

    Added vim git editor in nvim terminal

--- shell/bashrc.d/01-core.sh
+++ shell/bashrc.d/01-core.sh
@@ -5,6 +5,12 @@
 export EDITOR="nvim"
 export VISUAL="nvim"
 
+if [ -n "$NVIM" ]; then
+  export GIT_EDITOR=vim
+else
+  export GIT_EDITOR=nvim
+fi
+
 DEPRECATED_FUNCTIONS=(
   deploy
   bashrc-update
