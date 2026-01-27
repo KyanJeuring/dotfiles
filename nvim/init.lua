e549057be53c535f9566d2698e13395854d2dd76
Kyan Jeuring <kyanjeuring@gmail.com>
2026-01-27 14:08:01 +0100

    Fixed flickering

--- nvim/init.lua
+++ nvim/init.lua
@@ -122,13 +122,15 @@ vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 if not vim.env.SSH_TTY then
   vim.api.nvim_create_autocmd("WinEnter", {
     callback = function()
-      vim.wo.cursorline = true
+      if vim.bo.filetype ~= "NvimTree" then
+        vim.opt_local.cursorline = true
+      end
     end,
   })
 
   vim.api.nvim_create_autocmd("WinLeave", {
     callback = function()
-      vim.wo.cursorline = false
+      vim.opt_local.cursorline = false
     end,
   })
 end
